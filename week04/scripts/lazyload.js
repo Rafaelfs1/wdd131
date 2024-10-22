9480446088d8e1ed2bbc1d1a30dca78382dd9117
Rafaelfs1 <f4elrfs@gmail.com>
2024-10-21 22:37:26 -0300

    w4

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,8 @@
+// insert the current year and de copyright symbol
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `&copy ${today.getFullYear()}</span>`;
+
+// insert the date from the last modification was done
+const lastModified = document.lastModified;
+document.getElementById("lastmodified").textContent = lastModified;
